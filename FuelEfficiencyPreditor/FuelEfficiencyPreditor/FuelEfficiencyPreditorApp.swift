2f0f5323266794742116ec6c1ae5f5904154efea
Rakesh Yadav <rryadav142001@gmail.com>
2025-08-02 18:49:40 +0530

    Initial project setup

---
+++ FuelEfficiencyPreditor/FuelEfficiencyPreditor/FuelEfficiencyPreditorApp.swift
@@ -0,0 +1,17 @@
+//
+//  FuelEfficiencyPreditorApp.swift
+//  FuelEfficiencyPreditor
+//
+//  Created by Rakesh Yadav on 02/08/25.
+//
+
+import SwiftUI
+
+@main
+struct FuelEfficiencyPreditorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FuelEfficiencyPreditor/FuelEfficiencyPreditorTests/FuelEfficiencyPreditorTests.swift
@@ -0,0 +1,16 @@
+//
+//  FuelEfficiencyPreditorTests.swift
+//  FuelEfficiencyPreditorTests
+//
+//  Created by Rakesh Yadav on 02/08/25.
+//
+
+import Testing
+
+struct FuelEfficiencyPreditorTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
